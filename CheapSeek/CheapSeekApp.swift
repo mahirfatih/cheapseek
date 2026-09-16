63bd7fc83c779d8e1cafe9b26e4853b166e7e129
Mahir F. UNAL <irtibat@mahirfatih.com>
2026-09-17 00:38:57 +0300

    Wire transition notifications into the app and settings UI

--- CheapSeek/CheapSeekApp.swift
+++ CheapSeek/CheapSeekApp.swift
@@ -8,7 +8,7 @@ struct CheapSeekApp: App {
     init() {
         let settings = AppSettings()
         let config = DeepSeekConfig.load()
-        let model = AppModel(settings: settings, config: config)
+        let model = AppModel(settings: settings, config: config, notifications: NotificationManager())
         _settings = State(initialValue: settings)
         _model = State(initialValue: model)
     }
